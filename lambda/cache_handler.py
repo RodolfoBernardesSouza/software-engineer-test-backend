64a822706abb38b5c066b52867808c066bc4ca42
Rodolfo Souza <rodolfo.b2@gmail.com>
2023-03-02 10:19:12 -0300

    add of .gitinore

---
+++ lambda/cache_handler.py
@@ -0,0 +1,21 @@
+import json
+import logging
+import os
+
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def consumer(event, context):
+    logger.info(f'raw event: {event}')
+    for record in event['Records']:
+        logger.info(f'Message body: {record["body"]}')
+        #logger.info(f'Message attribute: {record["messageAttributes"]["AttributeName"]["stringValue"]}')
+        body = json.loads(record["body"])
+        name = body['name'] #validar o nome, o email e a senha ver se algum dos campos está vazio, dentro de um try catch
+        logger.info(name)
+
+
+
---
+++ lambda/message_handler.py
@@ -0,0 +1,18 @@
+import json
+import logging
+import os
+
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def consumer(event, context):
+    logger.info(f'raw event: {event}')
+    for record in event['Records']:
+        logger.info(f'Message body: {record["body"]}')
+        #logger.info(f'Message attribute: {record["messageAttributes"]["AttributeName"]["stringValue"]}')
+
+
+
