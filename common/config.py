a2e35e3d52306b3f07b975be163333b12f3dd20d
Rodolfo Souza <rodolfo.b2@gmail.com>
2023-03-03 21:08:57 -0300

    add of json validation

---
+++ common/config.py
@@ -0,0 +1,106 @@
+import logging
+from dataclasses import dataclass
+from os import getenv
+from logging import Logger
+
+
+@dataclass
+class Environment:
+    aws_region: str = getenv('REGION')
+    table_name: str = getenv('CACHE_TABLE')
+
+
+class CustomLog:
+
+    @staticmethod
+    def setup_logger() -> Logger:
+        formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        return logger
+
+
+@dataclass
+class JsonSchema:
+    # TODO add restrictions for empty atributes
+    user_schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "entityType": {"type": "string"},
+            "eventType": {"type": "string"},
+            "eventData": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string"},
+                    "email": {"type": "string"},
+                    "password": {"type": "string"},
+                    "groups": {"type": "array", "items": [{"type": "string"}, {"type": "string"}]}
+                },
+                "required": [
+                    "name",
+                    "email",
+                    "password",
+                    "groups"
+                ]
+            }
+        },
+        "required": [
+            "entityType",
+            "eventType",
+            "eventData"
+        ]
+    }
+
+    # TODO add restrictions for empty atributes
+    user_delete_schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "entityType": {"type": "string"},
+            "eventType": {"type": "string"},
+            "eventData": {
+                "type": "object",
+                "properties": {
+                    "email": {"type": "string"}
+                },
+                "required": [
+                    "email"
+                ]
+            }
+        },
+        "required": [
+            "entityType",
+            "eventType",
+            "eventData"
+        ]
+    }
+
+    # TODO add restrictions for empty atributes
+    group_schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "object",
+        "properties": {
+            "entityType": {"type": "string"},
+            "eventType": {"type": "string"},
+            "eventData": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string"}
+                },
+                "required": [
+                    "name"
+                ]
+            }
+        },
+        "required": [
+            "entityType",
+            "eventType",
+            "eventData"
+        ]
+    }
